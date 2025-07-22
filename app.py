4b1bba3aa56fd6d98c8864d9de7b2c22464f886b
Zenn9888 <nickchio30310@gmail.com>
2025-07-22 23:15:39 +0800

    deploy: upgrade to line-bot-sdk v3

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import re
+
 import json
 from flask import Flask, request, abort
 
@@ -28,7 +29,10 @@ CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
 CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
 GOOGLE_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")
 MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
-
+print("✅ CHANNEL_ACCESS_TOKEN:", os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
+print("✅ CHANNEL_SECRET:", os.getenv("LINE_CHANNEL_SECRET"))
+print("✅ GOOGLE_MAPS_API_KEY:", os.getenv("GOOGLE_MAPS_API_KEY"))
+print("✅ MONGO_URL:", os.getenv("MONGO_URL"))
 # ✅ 初始化 LINE Bot SDK v3
 configuration = Configuration(access_token=CHANNEL_ACCESS_TOKEN)
 line_bot_api = MessagingApi(configuration)
