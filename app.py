315eb34089b554e9aed5b3ef99c46ca466618866
Zenn9888 <nickchio30310@gmail.com>
2025-07-22 20:13:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -100,4 +100,4 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, flex)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
