196fc3343ceec7af1a5ab07ade64b2a8ed676bc9
Zenn9888 <nickchio30310@gmail.com>
2025-07-25 02:32:09 +0800

    deploy: upgrade to line-bot-sdk v3

--- app.py
+++ app.py
@@ -51,7 +51,9 @@ CHINESE_NAME_PATTERN = r'[\u4e00-\u9fff]{2,}'
 ADD_ALIASES = ["新增", "加入", "增加", "+", "加", "增"]
 DELETE_PATTERN = ["刪除", "移除", "del", "delete", "-", "刪", "移"]
 COMMENT_PATTERN = ["註解", "備註", "note", "comment", "註", "*"]
-
+@app.route("/ping", methods=["GET"])
+def ping():
+    return "pong", 200
 def clean_place_title(name):
     name = name.replace("+", " ")
     for delimiter in ['｜', '|', '-', '、', '(', '（']:
